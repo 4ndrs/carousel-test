22f6598009ef5be295039d515fad6901af7b5b82
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-30 18:47:28 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import styles from "./App.module.css";
+
+const images = [
+  "/ruby-pics/1.webp",
+  "/ruby-pics/2.webp",
+  "/ruby-pics/3.webp",
+  "/ruby-pics/4.webp",
+  "/ruby-pics/5.webp",
+];
+
+const App = () => {
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          width={280}
+          height={280}
+          alt={`Image ${index}`}
+          onClick={() => setSelectedImageIndex(index)}
+          className={`${styles.image} ${
+            index === selectedImageIndex ? styles.selected : ""
+          }`}
+          style={{
+            transform:
+              index === selectedImageIndex
+                ? ""
+                : `translateX(${(index - selectedImageIndex) * 150}%)`,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
